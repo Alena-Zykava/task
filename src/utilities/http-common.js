e1d9cd3674e8dc5dadc83b71f87230fde40a8de0
Zykava Alena <zykava.alena@gmail.com>
2021-06-16 17:29:28 +0300

    change url

--- src/utilities/http-common.js
+++ src/utilities/http-common.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const URL = 'https://thingproxy.freeboard.io/fetch/https://www.mrsoft.by/';
+// const URL = 'https://thingproxy.freeboard.io/fetch/https://www.mrsoft.by/';
+const URL = 'https://api.allorigins.win/raw?url=https://www.mrsoft.by'
 
 export const httpWords = axios.create({
     baseURL: URL,
